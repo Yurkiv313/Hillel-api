d7d0af721e97134a66ef63988df6bf21fdbb7968
Andriy Yurkiv <andriy.yurkiv@gmail.com>
2023-12-24 21:05:12 +0200

    requirements.txt

--- api_bart/settings.py
+++ api_bart/settings.py
@@ -80,7 +80,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = "api_bart.urls"
 
